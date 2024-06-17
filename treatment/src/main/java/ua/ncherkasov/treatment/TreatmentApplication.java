6647d0351edc543a4f2f06efb36a9fd0543cef3a
mik-od <mikola.cherkasov@gmail.com>
2024-06-17 17:48:37 +0300

    Add files via upload
    Initial commit

---
+++ treatment/src/main/java/ua/ncherkasov/treatment/TreatmentApplication.java
@@ -0,0 +1,13 @@
+package ua.ncherkasov.treatment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreatmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreatmentApplication.class, args);
+	}
+
+}
